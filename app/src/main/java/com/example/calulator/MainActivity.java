6399a5160cc56b7b7ba467422f627fd2b723bf45
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2020-02-18 13:18:31 +0100

    Finshed UwU

--- app/src/main/java/com/example/calulator/MainActivity.java
+++ app/src/main/java/com/example/calulator/MainActivity.java
@@ -10,10 +10,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView clacInputText;
+    TextView clacInputText, facitText;
     EditText calcInput;
     Button calcInputButton;
     Begregner begregner = new Begregner();
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +24,21 @@ public class MainActivity extends AppCompatActivity {
         clacInputText = (TextView) findViewById(R.id.clacText);
         calcInput = (EditText) findViewById(R.id.clacinput);
         calcInputButton = (Button) findViewById(R.id.facit);
+        facitText=(TextView) findViewById(R.id.facitText);
 
         calcInputButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                begregner.getFacit();
-                Log.d(TAG, "getfacit: "+begregner.getFacit());
+                String  input = calcInput.getText().toString();
+                int resultst = Integer.parseInt(input);
+                Log.d( TAG,"før: "+ begregner.getFacit());
+
+
+                facitText.setText(Integer.toString(begregner.addFacit(resultst)));
+                Log.d( TAG,"efter: "+ begregner.getFacit());
+
+
             }
         });
 
